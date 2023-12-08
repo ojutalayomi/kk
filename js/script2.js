460e6b8d91a2536a9f4ac1b2494d8f98523ab3e1
Ayomide Ojutalayo <118499752+ojutalayomi@users.noreply.github.com>
2023-12-08 22:43:06 +0100

    Add files via upload

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,10 @@
+var button = document.querySelectorAll(".button");
 var div = document.querySelectorAll("div");
 var loader = document.getElementById("loader");
 
-
+button.onclick = function() {
+    window.location.href = "./";
+}
 
 setTimeout(function() {
     loader.classList.remove("show");
